bcb26d4e40530436f639aad7163e7ccacd0dfbc7
John Harrison <johndharrison0@gmail.com>
2014-12-06 12:47:24 +0700

    Setup dockerAPI reference class.

---
+++ R/dockerAPI.R
@@ -0,0 +1,20 @@
+#' CLASS dockerAPI
+#'
+#' dockerAPI Class uses the 
+#' 
+#' dockerAPI is a generator object. 
+#'
+#' @import RJSONIO
+#' @import RCurl
+#' @export dockerAPI
+#' @exportClass dockerAPI
+#' @aliases dockerAPI
+#' @examples
+#' \dontrun{
+#' }
+
+dockerAPI <- remoteDriver <- setRefClass("remoteDriver",
+                                         fields = list(),
+                                         methods = list()
+)
+                                             
