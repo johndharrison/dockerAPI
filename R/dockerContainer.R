d2e9f671663e886a27ba145f3d12d4459dbcac68
John Harrison <johndharrison0@gmail.com>
2014-12-07 20:07:53 +0700

    Add an initialisation method to dockerContainer class.

--- R/dockerContainer.R
+++ R/dockerContainer.R
@@ -14,7 +14,26 @@
 #' }
 
 dockerContainer <- setRefClass("dockerContainer",
-                      fields = list(),
+                      fields = list(id = "character"
+                                    , created = "POSIXct"
+                                    , image = "character"
+                                    , names = "list"
+                                    , ports = "list"
+                                    , status = "character"
+                                    , command = "character"),
                       contains = "docker",
-                      methods = list()
+                      methods = list(
+                        initialize = function(id = "", created = NULL, image = NULL, names = list(), ports = list(), status = NULL, command = NULL, ...){
+                          #ports <- if(is.data.frame(ports)){ports}else{ports[[1]]}
+                          if(ncol(ports[[1]]) == 0){ports[[1]] <- data.frame(IP = character(0), PrivatePort = numeric(0), PublicPort = numeric(0), Type = character(0))}
+                          id <<- id
+                          created <<- created
+                          image <<- image
+                          names <<- names
+                          ports <<- ports
+                          status <<- status
+                          command <<- command
+                          callSuper(...)
+                        }
+                          )
 )
