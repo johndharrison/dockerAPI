9e11703e076a0d48ce8c52fd0f69314a48be796e
John Harrison <johndharrison0@gmail.com>
2014-12-07 15:31:39 +0700

    Add new class for tab completion. Remove reference to old dockerAPI class.

--- R/util.R
+++ R/util.R
@@ -1,8 +1,27 @@
-#' @export .DollarNames.dockerAPI
+#' @export .DollarNames.docker
+#' @export .DollarNames.dockerContainer
+#' @export .DollarNames.dockermage
 #' @import methods
 
-.DollarNames.dockerAPI <- function(x, pattern){
+.DollarNames.docker <- function(x, pattern){
   superMethods <- getRefClass("envRefClass")$methods()
+  errorMethods <- getRefClass("errorHandler")$methods()
   dockerMethods <- getRefClass(class(x))$methods()
-  grep(pattern, dockerMethods[!dockerMethods%in%superMethods], value=TRUE)
+  grep(pattern, dockerMethods[!dockerMethods%in%c(superMethods, errorMethods)], value=TRUE)
+}
+
+.DollarNames.dockerContainer <- function(x, pattern){
+  superMethods <- getRefClass("envRefClass")$methods()
+  errorMethods <- getRefClass("errorHandler")$methods()
+  dockerMethods <- getRefClass("docker")$methods()
+  dockerContainerMethods <- getRefClass(class(x))$methods()
+  grep(pattern, dockerContainerMethods[!dockerContainerMethods%in%c(superMethods, dockerMethods, errorMethods)], value=TRUE)
+}
+
+.DollarNames.dockerImage <- function(x, pattern){
+  superMethods <- getRefClass("envRefClass")$methods()
+  errorMethods <- getRefClass("errorHandler")$methods()
+  dockerMethods <- getRefClass("docker")$methods()
+  dockerImageMethods <- getRefClass(class(x))$methods()
+  grep(pattern, dockerImageMethods[!dockerImageMethods%in%c(superMethods, dockerMethods, errorMethods)], value=TRUE)
 }
