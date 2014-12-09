362f1f67cfa8329a56cabea03af6a67951b3e4cf
John Harrison <johndharrison0@gmail.com>
2014-12-09 16:42:08 +0700

    Add methods for imageList class to enable extraction and binding.

---
+++ R/imageList.R
@@ -0,0 +1,27 @@
+#' @export
+rbind.imageList <- function(x){
+  res <- lapply(x, function(y){
+    out <- lapply(c(id = "id", created = "created", parentId = "parentId",
+                    repoTags = "repoTags", size = "size", virtualSize = "virtualSize"), y$field)
+    out$repoTags <- I(out$repoTags)
+    data.frame(out, stringsAsFactors = FALSE)
+  }
+  )
+  do.call(rbind.data.frame, res)
+}
+
+#' @export
+print.imageList <- function(x, ...){
+  print(rbind(x), ...)
+}
+
+#' @export
+`[.imageList` <- function(x, i ,j){
+  if(missing(j)){
+    res <- .Primitive("[")(unclass(x), i)
+    return(`class<-`(res, "imageList"))
+  }else{
+    rbind(x)[i,j]
+  }
+  
+}
