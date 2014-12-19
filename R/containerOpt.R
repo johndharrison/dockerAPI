f60d7a6f7e505c7f1f1a39cd61acae559ba1ad79
John Harrison <johndharrison@gmail.com>
2014-12-18 18:34:21 -0800

    Add utility function containerOpt to aid construction of container.

---
+++ R/containerOpt.R
@@ -0,0 +1,13 @@
+#' Create container options
+#'
+#' \code{containerOpt}
+#' A utility function to create options suitable for creating a container.
+#' @export
+#' @examples
+#' \dontrun{
+#' containerOpt()
+#' }
+
+containerOpt <- function(){
+  
+}
